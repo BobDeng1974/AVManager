56b9039f28cf32a63927eeed6a539053ecf3fedb
alvin2010 <709699023@qq.com>
2017-07-28 15:29:31 +0800

    透明窗口实现

--- AVManager/LovingWnd.h
+++ AVManager/LovingWnd.h
@@ -1,42 +1,24 @@
 #ifndef LOVING_WND_H_
 #define LOVING_WND_H_
 
-class CLovingWnd : public CWindowWnd,
-	public IDialogBuilderCallback
+class CLovingWnd : public WindowImplBase
 {
 public:
 	CLovingWnd();
 	~CLovingWnd();
 
-	virtual LPCTSTR GetWindowClassName() const;
-	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
-public:
-	virtual UINT GetClassStyle() const;
-	virtual UILIB_RESOURCETYPE GetResourceType() const;
-	virtual CControlUI* CreateControl(LPCTSTR pstrClass);
-	//virtual LRESULT MessageHandler(UINT uMsg, WPARAM wParam, LPARAM /*lParam*/, bool& /*bHandled*/);
-	virtual LRESULT OnClose(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled);
-	virtual LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled);
-
-#if defined(WIN32) && !defined(UNDER_CE)
-	virtual LRESULT OnNcActivate(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*/, BOOL& bHandled);
-	virtual LRESULT OnNcCalcSize(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	virtual LRESULT OnNcPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
-	virtual LRESULT OnNcHitTest(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	virtual LRESULT OnGetMinMaxInfo(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	virtual LRESULT OnMouseWheel(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled);
-	virtual LRESULT OnMouseHover(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-#endif
-	virtual LRESULT OnSize(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	virtual LRESULT OnChar(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	virtual LRESULT OnSysCommand(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-	virtual LRESULT OnCreate(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
-
 	virtual CDuiString GetSkinFolder();
 	virtual CDuiString GetSkinFile();
+	virtual LPCTSTR GetWindowClassName(void) const;
+	virtual void InitWindow();
+
+	void Notify(TNotifyUI& msg);
+	CControlUI* CreateControl(LPCTSTR pstrClass);
+	LRESULT HandleCustomMessage(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
 
 private:
 	CPaintManagerUI m_PaintManager;
+	int m_nInc;
 };
 
 #endif
